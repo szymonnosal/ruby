02e05056a16986be47ba19d0e9b9103ecc2d0a41
szymonnosal <szymon.nosal@gmail.com>
2014-04-01 09:25:44 +0200

    Update C4Z5.rb

--- c4/C4Z5.rb
+++ c4/C4Z5.rb
@@ -1,11 +1,12 @@
 adresy_pl = File.new("adresyPL.txt", "w")
 adresy = File.new("adresy.txt", "w")
-file = File.open("date.txt", "r") do |file|
+file = File.open("dane.txt", "r") do |file|
 
 while tmp = file.gets
-  tmp = tmp.scan('[a-zA-Z0-9_]@[a-zA-Z0-9].[a-zA-Z]').join
+  tmp = tmp.scan('/[a-zA-Z0-9_]+@[a-zA-Z0-9]+.[a-zA-Z]+/').join
   
   if(tmp.length!=0)
     adresy.puts(tmp + ",")
   end
+end
 end
