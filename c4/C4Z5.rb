b935e12124d6c75e7246bbb9d4c9d27dab7a592f
szymonnosal <szymon.nosal@gmail.com>
2014-04-01 09:18:37 +0200

    Create C4Z5.rb

---
+++ c4/C4Z5.rb
@@ -0,0 +1,11 @@
+adresy_pl = File.new("adresyPL.txt", "w")
+adresy = File.new("adresy.txt", "w")
+file = File.open("date.txt", "r") do |file|
+
+while tmp = file.gets
+  tmp = tmp.scan('[a-zA-Z0-9_]@[a-zA-Z0-9].[a-zA-Z]').join
+  
+  if(tmp.length!=0)
+    adresy.puts(tmp + ",")
+  end
+end
