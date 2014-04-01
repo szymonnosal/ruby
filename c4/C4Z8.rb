393e384b51c04bee151b95cca84a51ccb39dcfea
szymonnosal <szymon.nosal@gmail.com>
2014-04-01 09:32:32 +0200

    Update C4Z8.rb

--- c4/C4Z8.rb
+++ c4/C4Z8.rb
@@ -1,7 +1,7 @@
     c = File.read('2.txt')
-     
+   
     for l in c.split("\n") do
-    l =~ /(.+) dług (\d+) oddał (\d+) /
-    d = $2 - $3
-    puts '#{$1} musi zwrocic #{d}'
+     l =~ /(.+) dlug (\d+) oddal (\d+)/
+     d = $2.to_i - $3.to_i
+     puts "#{$1} musi zwrocic #{d}"
     end
