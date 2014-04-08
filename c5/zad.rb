f990e8e930184a837c1bbf5423fd31bf5b13022f
szymonnosal <szymon.nosal@gmail.com>
2014-04-08 09:21:53 +0200

    Update zad.rb

--- c5/zad.rb
+++ c5/zad.rb
@@ -1,40 +1,51 @@
 require "mini_magick"
 
-def resize()
-	image = MiniMagick::Image.open("Lenna.png")
+def resize(img)
+	image = MiniMagick::Image.open(img)
 	image.resize "100x100"
 	image.write  "Lenna-resize.png"
 end
 
-def change_format()
-	image = MiniMagick::Image.open("Lenna.png")
+def change_format(img)
+	image = MiniMagick::Image.open(img)
 	image.resize "5x5"
 	image.format "jpg"
 	image.write "Lenna.jpg"
 end
 
-def watermark()
-	image = Image.open("Lenna.png")
-	result = image.composite(Image.open("watermark.png", "png")) do |c|
+def watermark(img)
+	image = MiniMagick::Image.open(img)
+	result = image.composite(MiniMagick::Image.open("Lenna-resize.png", "png")) do |c|
 	  c.gravity "center"
 	end
 	result.write "Lenna-watermark.png"
 end
 
-def grayscale()
-	image = MiniMagick::Image.open("Lenna.png")
+def grayscale(img)
+	image = MiniMagick::Image.open(img)
 	image.type "Grayscale"
 	image.write  "Lenna-gray.png"
 end
 
-def negative()
-	image = MiniMagick::Image.open("iLenna.png")
-	image.negative
-	image.write  "Lenna-negativ.png"
+def negative(img)
+	image = MiniMagick::Image.open(img)
+	image.negate()
+	image.write  "Lenna-negate.png"
 end
 
-resize()
-grayscale()
-negative()
-change_format()
-watermark()
+def rotate_image(img)
+	image = MiniMagick::Image.open(img)
+	image.combine_options do |c|
+	  c.rotate "-45>"
+	end
+	image.write "Lenna-rotate.png"
+end
+
+img = "Lenna.png"
+
+resize(img)
+grayscale(img)
+negative(img)
+change_format(img)
+watermark(img)
+rotate_image(img)
